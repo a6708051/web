1ee9339561b2bef9a6cb5fef8571869f7fca5689
kary_abc <530219765@qq.com>
2017-08-03 15:56:14 +0800

    modify foot line

--- Apps/Home/View/Public/footer.php
+++ Apps/Home/View/Public/footer.php
@@ -1,11 +1,12 @@
-<hr />
+<div class="footer-line"></div>
 <p class="footer-icp">备案号：粤ICP备15113383号</p>
 <style>
-hr {
-	width:80%;
-	height:2px;
-	border:none;
-	border-top:1px solid #ccc;"
+.footer-line {
+	width: 80%;
+	height: 2px;
+	border-top: 1px solid #ccc;
+	clear: both;
+	margin: 10px auto;
 }
 .footer-icp {
 	font-size: 12px;
